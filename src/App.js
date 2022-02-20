d40de19fccbec84c78de61bb2916aa7462e56cf5
Afara <afarasidheek@gmail.com>
2022-02-20 15:29:20 +0400

    API integrated

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import "./App.css";
 import Banner from "./Components/Banner/Banner";
 import NavBar from "./Components/NavBar/NavBar";
 import RowPost from "./Components/RowPost/RowPost";
-
+import { originals,action,comedy,romance ,horror} from "./urls";
 function App() {
   return (
     <div>
       <NavBar/>
       <Banner/>
-      <RowPost/>
+      <RowPost url={originals} title="Netflix originals"/>
+      <RowPost url={action} title="Action Movies" isSmall/>
+      <RowPost url={comedy} title="Comedy Movies" isSmall/>
+      <RowPost url={romance} title="Romance Movies" isSmall/>
+      <RowPost url={horror} title="Horror Movies" isSmall/>
     </div>
   );
 }
--- src/Components/RowPost/RowPost.js
+++ src/Components/RowPost/RowPost.js
@@ -1,32 +1,55 @@
 import React, { useEffect, useState } from "react";
 import "./RowPost.css";
 import axios from "../../axios";
-import {API_KEY,IMAGE_URL} from '../../constants/constants'
-function RowPost() {
+import {IMAGE_URL,API_KEY} from '../../constants/constants'
+import YouTube from "react-youtube";
+function RowPost(props) {
   const [movies, setMovies] = useState([])
+  const [urlId, setUrlId] = useState('')
   useEffect(() => {
-    axios.get(`discover/tv?api_key=${API_KEY}&with_networks=213`).then((response)=>{
+    axios.get(props.url).then((response)=>{
       console.log(response.data)
       setMovies(response.data.results)
     }).catch(err=>{
       alert('Network error')
     });
   }, []);
+  const opts = {
+    height: '390',
+    width: '100%',
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 1,
+    },
+  };
+
+  const handleMovies = (id)=>{
+    console.log(id)
+    axios.get(`movie/${id}/videos?api_key=${API_KEY}&language=en-US`).then((response)=>{
+      console.log(response.data)
+      if(response.data.results.length!==0 )
+      {
+        setUrlId(response.data.results[0])
+      }
+     else{
+       console.log('trailor  not available')
+     }
+    })
+  }
   return (
     <div className="row">
-      <h2>Netflix originals</h2>
+      <h2>{props.title}</h2>
 
       <div className="posters">
         {movies.map((obj)=>
-          <img
-          className="poster"
+          <img onClick={()=>handleMovies(obj.id)}
+          className={props.isSmall ? 'smallPoster' : 'poster'}
           alt="poster"
           src={`${IMAGE_URL+obj.backdrop_path}`}
         ></img>
         )}
-        
-        
       </div>
+       { urlId && <YouTube videoId={urlId.key} opts={opts} />}
     </div>
   );
 }
